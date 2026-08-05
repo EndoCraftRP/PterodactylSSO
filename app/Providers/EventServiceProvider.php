3529cfcf37f8d24e6d0a094994d804b9c25f9bbd
kuani <116524310+kuani50@users.noreply.github.com>
2026-08-05 21:36:17 +0200

    fix: import

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,11 +9,14 @@ use Pterodactyl\Models\EggVariable;
 use Pterodactyl\Observers\UserObserver;
 use Pterodactyl\Observers\ServerObserver;
 use Pterodactyl\Observers\SubuserObserver;
+use Pterodactyl\Listeners\TwoFactorListener;
+use Pterodactyl\Listeners\RevocationListener;
 use Pterodactyl\Observers\EggVariableObserver;
-use Pterodactyl\Listeners\Auth\AuthenticationListener;
+use Pterodactyl\Listeners\AuthenticationListener;
 use Pterodactyl\Events\Server\Installed as ServerInstalledEvent;
 use Pterodactyl\Notifications\ServerInstalled as ServerInstalledNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
 
 class EventServiceProvider extends ServiceProvider
 {
