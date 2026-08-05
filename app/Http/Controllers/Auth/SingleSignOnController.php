5c5239125d3cccaecf7de1798a92913c82a624cc
kuani <116524310+kuani50@users.noreply.github.com>
2026-08-05 21:39:01 +0200

    Remove account creation from SSO controller
    Removed account creation logic from SingleSignOnController.

--- app/Http/Controllers/Auth/SingleSignOnController.php
+++ app/Http/Controllers/Auth/SingleSignOnController.php
@@ -22,30 +22,9 @@ class SingleSignOnController extends AbstractLoginController
         if (User::where('email', '=', $service->getEmail())->exists()) {
             $getUser = User::where('email', $service->getEmail())->first();
             Auth::loginUsingId($getUser->id);
-        } else {
-            if(config('sso.create_account')) {
-                $this->CreateNewAccount($service);
-            }
         }
-
-        return redirect('/');
-    }
-
-    private function CreateNewAccount($service)
-    {
-        $email = $service->getEmail();
-        if(!$service->getNickname()) { $username =  $service->getName(); } else { $username =  $service->getNickname(); }
         
-        $username = str_replace(' ', '', $username);
-        $username = preg_replace("/[^a-zA-Z0-9]+/", "", $username);
-
-
-        //TODO Add admin auth based on user groups: $userGroups = $service->groups;
-
-        $res = Artisan::call("p:user:make --email={$email} --username={$username} --name-first={$username} --name-last={$username} --admin=0 --no-password");
-        $getUser = User::where('email', $service->getEmail())->first();
-        Auth::loginUsingId($getUser->id);
-
+        return redirect('/');
     }
 
 
