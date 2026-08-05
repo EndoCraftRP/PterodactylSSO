cb8d834b8c6c1055cb10f1024eb6bf755e8b4fce
kuani <116524310+kuani50@users.noreply.github.com>
2026-08-05 21:22:03 +0200

    Update LoginContainer.tsx

--- resources/scripts/components/auth/LoginContainer.tsx
+++ resources/scripts/components/auth/LoginContainer.tsx
@@ -3,11 +3,9 @@ import { RouteComponentProps } from 'react-router-dom';
 
 const LoginContainer = ({ history }: RouteComponentProps) => {
     useEffect(() => {
-        // Redirige vers '/auth/login/sso' lorsque le composant est monté
         window.location.href = '/auth/login/sso';
     }, []);
 
-    // Ne retourne aucun contenu
     return null;
 };
 
